a51afaa2a16cbe33af6a1cc8f4366f2490a8c5f4
Will Coughlin <willcoughlin@gmail.com>
2024-07-09 22:53:20 -0500

    Improve tausworthe and clean up notebooks

--- generators/tausworthe.py
+++ generators/tausworthe.py
@@ -1,35 +1,42 @@
 class Tausworthe:
-  def __init__(self, r, q, l, seed=31):
+  def __init__(self, r, q, l, seed):
     self.r = r
     self.q = q
     self.l = l
 
-    self.i = self.q
+    self.trim_mask = int('1'*2*l, 2) # Int bitmask to cap state int at 2l bits.
+    self.mask = int('1'*self.l, 2)  # Int bit mask to extract l bits at a time from current state
+    self.state = seed #^ self.mask   # Use mask + seed to initialize state. Stores current bits stored as an integer.
 
-    # Convert seed to q-length bit list
-    seed_str = f'{seed % 2**q:0{q}b}'
-    self.bits = list(map(int, seed_str))
-
-  def __bits_to_int(self, bits_to_convert):
-    bit_str = ''.join(map(str, bits_to_convert))
-    return int(bit_str, 2)
-  
   def __increment_bits(self):
-    stop = self.i+self.l
-    while self.i < stop:
-      self.bits.append(self.bits[self.i-self.r] ^ self.bits[self.i-self.q])
-      self.i += 1
+    # Append l new bits to end of current state integer
+    for _ in range(self.l):
+      b1 = (self.state >> (self.r-1)) & 1  # bit i-r
+      b2 = (self.state >> (self.q-1)) & 1  # bit i-q
+      new_bit = b1 ^ b2
+      self.state = (self.state << 1) | new_bit  # shift and put new bit in the LSB 
+      self.state &= self.trim_mask
 
   def next(self):
-    bits_to_convert = self.bits[self.i-self.l-1:self.i-1]
     self.__increment_bits()
-
-    numerator = self.__bits_to_int(bits_to_convert)
+    numerator = self.state & self.mask
     return numerator / 2**self.l
 
-  def get_bits(self): return self.bits
-
 if __name__ == '__main__':
+  # Uncomment for simple run
   gen = Tausworthe(r=3, q=5, l=4, seed=31)
-  for _ in range(10):
+  for _ in range(20):
     print(gen.next())
+
+
+  # Uncomment for large timed run
+  # gen = Tausworthe(r=8, q=15, l=32, seed=0xdeadbeef)
+  # def time_generator(gen_func, N, M):
+  #   import timeit
+  #   trials = timeit.repeat(stmt='gen_func()', number=N, repeat=M, globals={'gen_func': gen_func})
+  #   min_trial = min(trials)
+  #   return min_trial * 1000, min_trial * 1000 / N
+  # N = 1000000
+  # M = 5
+  # print(time_generator(gen.next, N, M))
+
