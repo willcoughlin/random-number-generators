1e8e7ec640c9694a7cae35d7075aa1580b470886
Will Coughlin <willcoughlin@gmail.com>
2024-07-09 23:05:47 -0500

    Improve tausworthe runtime

--- generators/tausworthe.py
+++ generators/tausworthe.py
@@ -11,11 +11,13 @@ class Tausworthe:
   def __increment_bits(self):
     # Append l new bits to end of current state integer
     for _ in range(self.l):
-      b1 = (self.state >> (self.r-1)) & 1  # bit i-r
-      b2 = (self.state >> (self.q-1)) & 1  # bit i-q
+      # b1 = (self.state >> (self.r-1)) & 1  # bit i-r
+      # b2 = (self.state >> (self.q-1)) & 1  # bit i-q
+      b1 = 1 if self.state & (1 << self.r-1) else 0
+      b2 = 1 if self.state & (1 << self.q-1) else 0
       new_bit = b1 ^ b2
       self.state = (self.state << 1) | new_bit  # shift and put new bit in the LSB 
-      self.state &= self.trim_mask
+    self.state &= self.trim_mask
 
   def next(self):
     self.__increment_bits()
@@ -24,19 +26,19 @@ class Tausworthe:
 
 if __name__ == '__main__':
   # Uncomment for simple run
-  gen = Tausworthe(r=3, q=5, l=4, seed=31)
-  for _ in range(20):
-    print(gen.next())
+  # gen = Tausworthe(r=3, q=5, l=4, seed=31)
+  # for _ in range(20):
+  #   print(gen.next())
 
 
   # Uncomment for large timed run
-  # gen = Tausworthe(r=8, q=15, l=32, seed=0xdeadbeef)
-  # def time_generator(gen_func, N, M):
-  #   import timeit
-  #   trials = timeit.repeat(stmt='gen_func()', number=N, repeat=M, globals={'gen_func': gen_func})
-  #   min_trial = min(trials)
-  #   return min_trial * 1000, min_trial * 1000 / N
-  # N = 1000000
-  # M = 5
-  # print(time_generator(gen.next, N, M))
+  gen = Tausworthe(r=8, q=15, l=32, seed=0xdeadbeef)
+  def time_generator(gen_func, N, M):
+    import timeit
+    trials = timeit.repeat(stmt='gen_func()', number=N, repeat=M, globals={'gen_func': gen_func})
+    min_trial = min(trials)
+    return min_trial * 1000, min_trial * 1000 / N
+  N = 10000
+  M = 5
+  print(time_generator(gen.next, N, M))
 
